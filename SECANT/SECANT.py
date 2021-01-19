cb1bb119403b9a547b95a28545e5946bcc800343
Xinjun Wang <50209236+tarot0410@users.noreply.github.com>
2021-01-19 17:47:38 -0500

    Update SECANT.py

--- SECANT/SECANT.py
+++ SECANT/SECANT.py
@@ -185,121 +185,119 @@ def SECANT_CITE(data0, numCluster, cls, learning_rate=0.01, maxIter=500, earlyst
 
     return lbl.view(N), conMtxFinal, mu_out, scale3D_out, log_posteriors_final, logLikVec, logLik_final
 
-# compute logLikelihood of observed data for two datasets, one with labels and the other doesn't case
-def fullLogLik2(data0, data1, tauVec0, tauVec1, muMtx, conMtx, cov3D, classLabel_array, K, N0, N1):  
-    l0, deltaMtx = logLikDelta(data0, tauVec0, muMtx, cov3D, K, N0)
-    conMtxFull = torch.empty(N0, K, dtype = torch.float64).to(device)
-    deltaMtxT = torch.transpose(deltaMtx,0,1)
-    for i in range(N0):
-        conMtxFull[i,:] = conMtx[classLabel_array[i].astype(int),:] 
-        
-    tempMtx = torch.mm(conMtxFull, deltaMtxT)
-    l1 = logLik(data1, tauVec1, muMtx, cov3D, K, N1)
-    return torch.trace(torch.log(tempMtx)) + l0 + l1
+  # compute logLikelihood of observed data for two datasets, one with labels and the other doesn't case
+# data0: CITE-seq data (with ADT confident cell type)
+# data1: scRNA-seq data (without ADT info)
+def fullLogLik2(data0, data1, conMtx, muMtx, scale3D, cls, K, N0, N1): 
+    # Part1: concordance matrix and likelihood for data0
+    log_likelihoods0 = get_likelihoods(data0, muMtx, scale3D, log=True)
+    log_posteriors0 = get_posteriors(log_likelihoods0)
+    logP, ind0 = torch.max(log_posteriors0, 0, keepdim=True)
+    l0 = torch.sum(log_likelihoods0[ind0, range(log_likelihoods0.size()[1])])
+
+    cls_long = cls.long()
+    conMtxFull = conMtx[cls_long, :]
+
+    tempMtx = torch.mm(conMtxFull, torch.exp(log_posteriors0))
+    temp = torch.trace(torch.log(tempMtx + 1e-5))
+
+    # Part2: likelihood for data1
+    log_likelihoods1 = get_likelihoods(data1, muMtx, scale3D, log=True)
+    log_posteriors1 = get_posteriors(log_likelihoods1)
+    _, ind1 = torch.max(log_posteriors1, 0, keepdim=True)
+    l1 = torch.sum(log_likelihoods1[ind1, range(log_likelihoods1.size()[1])])
+    return temp + l0 + l1
 
 # Set up optimization algorithm for two datasets case
-def optimDL2(parameters, data0, data1, conMtx_temp, C, K, P, N0, N1, cls_np, learning_rate, nIter):
+def optimDL2(parameters, data0, data1, conMtx_temp, C, K, P, N0, N1, cls, learning_rate, maxIter, earlystop):
     # Defines a SGD optimizer to update the parameters
     optimizer = optim.Rprop(parameters, lr=learning_rate) 
 
-    pVec, tauVec0, tauVec1, muMtx, scale_3D = parameters
-    
-    for i in range(nIter):
+    tril_indices = torch.tril_indices(row=P, col=P, offset=0)
+    pVec, muMtx, lowtri_mtx = parameters
+    logLikVec = np.zeros(maxIter)
+
+    for i in range(maxIter):
         optimizer.zero_grad()
 
         # set up transformed p vector
         pVec_tran = dist.biject_to(dist.Binomial.arg_constraints['probs'])(pVec)
-
         # set up transformed concordance matrix
-        conMtx_tran = torch.empty(C, K, dtype = torch.float64).to(device)
+        conMtx_tran = torch.empty(C, K, dtype = torch.float32).to(device)
         conMtx_tran[0:(C-1),:] = conMtx_temp*pVec_tran
         conMtx_tran[C-1,:] = 1-pVec_tran
 
-        # set up transformed tau vector
-        tauVec_tran0 = dist.biject_to(dist.Multinomial.arg_constraints['probs'])(tauVec0)
-        tauVec_tran1 = dist.biject_to(dist.Multinomial.arg_constraints['probs'])(tauVec1)
-
         # set up transformed cov3D matrix
-        cov3D_tran = torch.empty(K, P, P, dtype = torch.float64).to(device)
-        for m in range(K):
-            cov3D_chol_m = dist.transform_to(dist.constraints.lower_cholesky)(scale_3D[m])
-            cov3D_tran[m] = torch.mm(cov3D_chol_m, cov3D_chol_m.t())
-
-        # Define loss function xMtx, piVec, alphaMtx, K
-        NLL = - fullLogLik2(data0, data1, tauVec_tran0, tauVec_tran1, muMtx, conMtx_tran, cov3D_tran, cls_np, K, N0, N1)
+        scale3D = torch.zeros(K, P, P, dtype = torch.float32).to(device)
+        scale3D[:, tril_indices[0], tril_indices[1]] = lowtri_mtx
+        scale3D[:, range(P), range(P)] = abs(scale3D[:, range(P), range(P)])
         
-        if i % 20 == 0:
-            print("")
-            print(i, "loglik  =", -NLL.cpu().data.numpy())
-            print("conMtx:")
-            print(np.around(conMtx_tran.cpu().data.numpy(),3))
-            print("tauVec0:")
-            print(np.around(tauVec_tran0.cpu().data.numpy(),3))
-            print("tauVec1:")
-            print(np.around(tauVec_tran1.cpu().data.numpy(),3))
+        # Define loss function xMtx, piVec, alphaMtx, K
+        NLL = - fullLogLik2(data0, data1, conMtx_tran, muMtx, scale3D, cls, K, N0, N1)
+        logLikVec[i] = -NLL
+
+        if i % 50 == 0:
+            print(i, "th iter...")
+            if (i > 0) &  (abs((logLikVec[i]-logLikVec[i-50]-1e-5)/(logLikVec[i-50]+1e-5)) < earlystop):
+                break
         
         NLL.backward()
         optimizer.step()
-        
-    return conMtx_tran, tauVec_tran0, tauVec_tran1, muMtx, cov3D_tran, -NLL
+    
+    return conMtx_tran, muMtx, scale3D, logLikVec, -NLL
 
 # Final function to run algorithm for one dataset case
-def SECANT_joint(data0, data1, numCluster, K, cls_np, uncertain = True, learning_rate=0.01, nIter=100, init_seed=2020):
+def SECANT_JOINT(data0, data1, numCluster, cls, learning_rate=0.01, maxIter=500, earlystop = 0.0001, n_gmm_init=5, init_seed=2020):
     torch.manual_seed(init_seed)
     
     N0 = data0.size()[0]
     N1 = data1.size()[0]
     P = data0.size()[1] 
-    
-    if uncertain:
-        C = np.unique(cls_np).size
-    else:
-        C = np.unique(cls_np).size+1
-    
+    C = len(torch.unique(cls))
+    K = sum(numCluster)
+
     # concordance p vector
-    p_init = torch.ones(K, dtype = torch.float64)*0.5
+    p_init = torch.ones(K, dtype = torch.float32) *0.5
     pVec = dist.biject_to(dist.Binomial.arg_constraints['probs']).inv(p_init)
     
     # clustering parameters
-    conMtx_temp, mu_init, tau_init = initParam(data0, numCluster, C, K, P, cls_np, init_seed)
+    conMtx_temp, mu_init, cov_diag_init = initParam(data0, numCluster, C, K, P, cls, n_gmm_init, init_seed)
 
     # initial cov3D
-    scale3D = torch.empty(K, P, P, dtype = torch.float64).to(device)
-    for k in range(K):
-        scale3D[k] = torch.eye(P, dtype = torch.float64).to(device)*0.01
+    tril_indices = torch.tril_indices(row=P, col=P, offset=0)
+    cov_diag_init = cov_diag_init.to(device)
+    temp0 = torch.eye(P, dtype=torch.float32).to(device)
+    temp0 = temp0.reshape((1, P, P))
+    temp1 = temp0.repeat(K, 1, 1)
+    cov_diag_init = cov_diag_init.view(K,P,1)
+    cov_int = temp1 * cov_diag_init
 
-    tauVec0 = dist.biject_to(dist.Multinomial.arg_constraints['probs']).inv(tau_init)
-    tauVec1 = dist.biject_to(dist.Multinomial.arg_constraints['probs']).inv(tau_init)
+    cov3D_decomp = torch.cholesky(cov_int)
+    lowtri_mtx = cov3D_decomp[:, tril_indices[0], tril_indices[1]]
+    
     muMtx = mu_init.clone()
 
     pVec = pVec.to(device)
-    tauVec0 = tauVec0.to(device)
-    tauVec1 = tauVec1.to(device)
     muMtx = muMtx.to(device)
-
+    lowtri_mtx = lowtri_mtx.to(device)
+    
     pVec.requires_grad = True
-    tauVec0.requires_grad = True
-    tauVec1.requires_grad = True
     muMtx.requires_grad = True
+    lowtri_mtx.requires_grad = True
 
-    scale3D = scale3D.to(device)
-    scale3D.requires_grad = True
-    param = [pVec, tauVec0, tauVec1, muMtx, scale3D]
-
-    conMtxFinal, tauVec0Final, tauVec1Final, muMtxFinal, cov3DFinal, loglikFinal = optimDL2(param, data0, data1, conMtx_temp, C, K, P, N0, N1, cls_np, learning_rate, nIter)
-
-    l0, deltaMtxFinal0 = logLikDelta(data0, tauVec0Final, muMtxFinal, cov3DFinal, K, N0)
-    outLbl0 = torch.zeros(N0)
-    for i in range(N0):
-        values, indices = torch.max(deltaMtxFinal0[i,:], 0)
-        outLbl0[i] = indices
-
-    l1, deltaMtxFinal1 = logLikDelta(data1, tauVec1Final, muMtxFinal, cov3DFinal, K, N1)
-    outLbl1 = torch.zeros(N1)
-    for j in range(N1):
-        values, indices = torch.max(deltaMtxFinal1[j,:], 0)
-        outLbl1[j] = indices
+    param = [pVec, muMtx, lowtri_mtx]
     
-    preditADT = torch.mm(conMtxFinal, deltaMtxFinal1.t())
+    conMtxFinal, mu_out, scale3D_out, logLikVec, logLik_final = optimDL2(param, data0, data1, conMtx_temp, C, K, P, N0, N1, cls, learning_rate, maxIter, earlystop)
+
+    logLik_temp0 = get_likelihoods(data0, mu_out, scale3D_out, log=True)
+    log_posteriors_final0 = get_posteriors(logLik_temp0)
+    logP0, lbl0 = torch.max(log_posteriors_final0, 0, keepdim=True)
+
+    logLik_temp1 = get_likelihoods(data1, mu_out, scale3D_out, log=True)
+    log_posteriors_final1 = get_posteriors(logLik_temp1)
+    logP1, lbl1 = torch.max(log_posteriors_final1, 0, keepdim=True)
     
-    return outLbl0, outLbl1, preditADT, conMtxFinal, tauVec0Final, tauVec1Final, muMtxFinal, cov3DFinal, loglikFinal
+    preditADT_post = torch.mm(conMtxFinal, torch.exp(log_posteriors_final1))
+    _, lbl_predict = torch.max(preditADT_post, 0, keepdim=True)
+    
+    return lbl0.view(N0), lbl1.view(N1), lbl_predict.view(N1), conMtxFinal, mu_out, scale3D_out, log_posteriors_final0, log_posteriors_final1, preditADT_post, logLikVec, logLik_final
